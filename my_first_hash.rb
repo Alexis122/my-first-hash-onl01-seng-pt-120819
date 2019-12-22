709356934490decd26cbe1b2f8abf8b82d46f63a
Alexis Priore <alexispriore@gmail.com>
2019-12-22 20:13:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,7 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+ hash = { "my_name" => "Alexis", "my_age" => "27" }
 end
+  
 
 
 def shipping_manifest
